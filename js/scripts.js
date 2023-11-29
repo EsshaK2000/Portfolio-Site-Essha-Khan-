3c0fcb6c069bc4c76a9524a9fe7425061dad76a2
EsshaK2000 <esshakhan2000@gmail.com>
2023-11-29 01:12:07 -0500

    Update to task 1.8

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,10 @@
-scripts.js
 console.log("Hello World");
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    x.className += ' responsive';
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+      } else {
+        x.className = 'navtoggle';
+      }
+}
